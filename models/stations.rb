00772714422ccf0d543e3919fda214b11bfff81b
Robert-Rino <a901002666@gmail.com>
2017-04-16 23:02:31 +0800

    station mongoid model

---
+++ models/stations.rb
@@ -0,0 +1,13 @@
+require 'mongoid'
+
+class Station
+  include Mongoid::Document
+  field :sno, type: String
+  field :name, type: String
+  field :tot, type: Integer
+  field :sbi, type: Integer
+  field :lat, type: Float
+  field :lng, type: Float
+  field :mday, type: Integer
+  field :updatetime, type:Integer, default: Time.now.to_i
+end
