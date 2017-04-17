e525317be6bba03ff5cdf9c83d1a4506de57d574
Robert-Rino <a901002666@gmail.com>
2017-04-18 00:49:50 +0800

    change city check solution

--- helpers/is_in_taipei_helper.rb
+++ helpers/is_in_taipei_helper.rb
@@ -4,7 +4,8 @@ class IsInTaipei
   def self.inTaipei(lan,lng)
     url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=#{lan},#{lng}&sensor=true"
     response = HTTParty.get(url)
-    city = response["results"][1]["address_components"][0]["long_name"].to_s
-    city == "Taipei" ? true : false
+    city = response["results"][0]["address_components"][-3]["long_name"].to_s
+    print city
+    city == "Taipei City" ? true : false
   end
 end
